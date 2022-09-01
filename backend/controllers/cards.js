2259e019e49009fd722681cdd1aa14931a39b111
sereja-art <sereja.artemov.art@yandex.ru>
2022-09-01 22:33:25 +0300

    fix: бэк теперь возвращает объект карточки вида: {}, а не card: {}

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -12,13 +12,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   cardModel.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send({
-      card: {
-        name: card.name,
-        link: card.link,
-        _id: card._id,
-      },
-    }))
+    .then((card) => res.status(200).send(card))
     .catch(next);
 };
 
