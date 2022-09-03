ae5fcea0306d42c12415f79ca58c09f421389d3b
sereja-art <sereja.artemov.art@yandex.ru>
2022-09-03 21:45:12 +0300

    Merge remote-tracking branch 'origin/main'

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,11 +4,18 @@ const UnauthorizedError = require('../error/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+  // const token = req.cookies.jwt;
+  // let payload;
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    // payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
   } catch (err) {
     throw next(new UnauthorizedError('Необходима авторизация'));
   }
