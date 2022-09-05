d047c8d551aba3c8cce4170d50c85d14c9a4738b
sereja-art <sereja-art-artemov@yandex.ru>
2022-09-05 16:04:27 +0300

    пробую на вм

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw next(new UnauthorizedError('Необходима авторизация'));
   }
